9edfbb563f5b82dac3b7f938ca45378b728ca725
basoffr <basoffringa1@gmail.com>
2025-05-29 15:27:38 +0200

    chore: declare VITE_SUPABASE_* env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_ANON_KEY: string;
+    // voeg hier meer VITE_... var's toe als je ze later nodig hebt
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
